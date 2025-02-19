0075e3b13a696b6c7897275d4651a1f7d3de5a49
gks14999 <gks14999@gmail.com>
2025-02-19 12:38:00 +0000

    pages init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Route } from 'react-router-dom';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 const App = () => {
   
   return (
-    <div className='text-green-500'>this is start and end</div>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path = '/about' element={<About/>}/>
+        <Route path = '/contact' element={<Contact/>}/>
+      </Routes>
+      {console.log(About)}
+    </div>
   );
 }
 
